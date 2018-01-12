ba684e0ebef2652c2853cd9c17f560aebf0761e9
kevin <web_stark@163.com>
2018-01-12 15:53:32 +0800

    Signed-off-by: kevin <web_stark@163.com>

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <child :content="msg"></child>
+  </div>
+</template>
+<script>
+  import child from './components/Child.vue'
+  export default{
+    name: 'app',
+    data () {
+      return {
+        'msg': '这是来自父组件的问候'
+      }
+    },
+    components: {
+      child
+    }
+  }
+</script>
+<style>
+
+</style>
---
+++ src/components/Child.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h3>这是子组件</h3>
+    <p>{{content}}</p>
+  </div>
+</template>
+<script>
+  export default{
+    props: ['content']
+  }
+</script>
