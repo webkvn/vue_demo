269b43d0fb77c3a1d76093146e664f298279ff14
xiongkai <xiongkai01@wondersgroup.com>
2019-03-29 16:07:38 +0800

    1607

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -57,19 +57,60 @@ export default {
     // });
     // console.log(flag);
 
-    var flag = _us.some([1, 2, 3, 4, 5, 6], function(num) {
-      return num % 2 == 0;
-    });
-    console.log(flag);
+    // var flag = _us.some([1, 2, 3, 4, 5, 6], function(num) {
+    //   return num % 2 == 0;
+    // });
+    // console.log(flag);
 
-    var size = _us.size({
-      one: 1,
-      two: 2,
-      three: 3
-    });
-    console.log(size);
+    // var size = _us.size({
+    //   one: 1,
+    //   two: 2,
+    //   three: 3
+    // });
+    // console.log(size);
 
+    // var arr = _us.filter([1, 2, 3, 4, 5, 6], function(num) {
+    //   return num % 2 == 0;
+    // });
+    // console.table(arr);
 
+    // var arr = _us.reject([1, 2, 3, 4, 5, 6], function(num) {
+    //   return num % 2 == 0;
+    // });
+    // console.table(arr);
+
+    // var result = _us.find([1, 2, 3, 4, 5, 6], function(num) {
+    //   return num % 2 == 0;
+    // });
+    // console.log(result);
+
+    // var result = _us.contains([1, 2, 3], 3);
+    // console.log(result);
+
+    // var obj = _us.countBy([1, 2, 3, 4, 5], function(num) {
+    //   return num % 2 == 0 ? 'even' : 'odd';
+    // });
+    // console.table(obj);
+
+    // var arr = _us.toArray({
+    //   a: 0,
+    //   b: 1,
+    //   c: 2
+    // });
+    // console.table(arr);
+
+    // var stooges = [{
+    //   name: 'moe',
+    //   age: 40
+    // }, {
+    //   name: 'larry',
+    //   age: 50
+    // }, {
+    //   name: 'curly',
+    //   age: 60
+    // }];
+    // var arr = _us.pluck(stooges, 'name');
+    // console.table(arr);
 
   }
 }
