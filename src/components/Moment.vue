ed0aef0d5899edb0aa304689d31f54b562e1994c
xiongkai <xiongkai01@wondersgroup.com>
2019-04-03 14:01:26 +0800

    moment

---
+++ src/components/Moment.vue
@@ -0,0 +1,31 @@
+<template>
+<div>
+  <p>Moment</p>
+</div>
+</template>
+<script>
+export default {
+  name: 'Moment',
+  data() {
+    return {
+      'msg': ''
+    }
+  },
+  methods: {
+    func() {
+      console.log(111)
+    }
+  },
+  created() {
+    const _today = this._mt();
+    console.log(_today.format('YYYY'));
+    console.log(_today.format('MM'));
+    console.log(_today.format('DD'));
+    console.log(_today.format());
+    console.log(_today.format('YYYY-MM-DD'));
+  }
+}
+</script>
+<style>
+
+</style>
